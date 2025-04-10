91eafb77aba787cd50a18d5af0d8440e39906d33
Wissarut Kanasub <wissarut.dev@gmail.com>
2025-04-10 23:09:26 +0700

    Setup theming with shadcnUI

--- frontend-nexjs/src/app/layout.tsx
+++ frontend-nexjs/src/app/layout.tsx
@@ -1,35 +1,46 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
+import {ThemeProvider} from "@/components/theme-provider"
 import "./globals.css";
 import React from "react";
 
+
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Todo List",
-  description: "Produced by Wissarut Kanasub",
+    title: "Todo List",
+    description: "Produced by Wissarut Kanasub",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <>
+            <html lang="en" suppressHydrationWarning>
+                <body
+                    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                >
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+                </body>
+            </html>
+        </>
+    );
 }
--- frontend-nexjs/src/app/page.tsx
+++ frontend-nexjs/src/app/page.tsx
@@ -1,7 +1,14 @@
+import {ModeToggle} from "@/components/theme/ModeToggle";
+import {Button} from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <div>
-      hello Wissarut Kanasub
-    </div>
-  );
+    return (
+        <div>
+            hello Wissarut Kanasub
+            <ModeToggle/>
+            <Button>
+                Hello Button!
+            </Button>
+        </div>
+    );
 }
