cbd5677ec87201045c8e52e5155d271d0e74c17c
Wissarut Kanasub <wissarut.dev@gmail.com>
2025-04-15 16:41:40 +0700

    Delete unused var

--- frontend-nextjs/src/app/signin/page.tsx
+++ frontend-nextjs/src/app/signin/page.tsx
@@ -1,11 +1,8 @@
 import Link from "next/link"
-import { ArrowLeft } from "lucide-react"
-
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Checkbox } from "@/components/ui/checkbox"
 
 export default function SignInPage() {
     return (
--- frontend-nextjs/src/app/signup/page.tsx
+++ frontend-nextjs/src/app/signup/page.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link"
-import { ArrowLeft } from "lucide-react"
-
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
